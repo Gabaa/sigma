18d713f38216d3b70e9914655e2b408999619639
Mikkel Erfurth Gaba <au615597@uni.au.dk>
2022-07-27 08:10:50 +0200

    Remove identity bins

--- src/bin/identityresponder.rs
+++
@@ -1,9 +0,0 @@
-use std::{io, net::TcpListener};
-
-fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("localhost:0")?;
-
-    loop {
-        let (stream, addr) = listener.accept()?;
-    }
-}
